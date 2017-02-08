d3ec14f9c2af74c37d913a7d5db1c1d15fe9ae06
Palmer-JC <jeffrey.c.palmer@gmail.com>
2017-02-08 15:00:31 -0500

    QI version 1.1 preliminary-
    AudioRecorder now supports down sampling

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -1,105 +1,168 @@
 var gulp  = require("gulp");
 var rename = require("gulp-rename");
 var uglify = require("gulp-uglify");
+var optimisejs = require('gulp-optimize-js');
+
+var path;
 
 gulp.task('armature' ,function() {
-	return gulp.src(["./scenes/QueuedInterpolation/armature/javascript/armature.js"])
+	path = "../scenes/QueuedInterpolation/armature/javascript";
+	return gulp.src([path + "/armature.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('boo' ,function() {
-	return gulp.src(["./scenes/boo/javascript/boo.js"])
+	path = "../scenes/boo/javascript";
+	return gulp.src([path + "/boo.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('camera_anim' ,function() {
-	return gulp.src(["./scenes/camera_anim/javascript/camera_anim.js"])
+	path = "../scenes/camera_anim/javascript";
+	return gulp.src([path + "/camera_anim.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('dead_baby' ,function() {
-	return gulp.src(["./scenes/game_rig_tester/javascript/dead_baby.js"])
+	path = "../scenes/QueuedInterpolation/game_rig_tester/javascript";
+	return gulp.src([path + "/dead_baby.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('eye_model' ,function() {
-	return gulp.src(["./scenes/eye_model/javascript/eye_model.js"])
+	path = "../scenes/Being/eye_model/javascript";
+	return gulp.src([path + "/eye_model.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
+});
+
+gulp.task('exact_scale_model' ,function() {
+	path = "../scenes/QueuedInterpolation/game_rig_tester/javascript";
+	return gulp.src([path + "/exact_scale.js"])
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('female_runner' ,function() {
-	return gulp.src(["./scenes/game_rig_tester/javascript/female_runner.js"])
+	path = "../scenes/QueuedInterpolation/game_rig_tester/javascript";
+	return gulp.src([path + "/female_runner.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
-gulp.task('finger_shapekeys' ,function() {
-	return gulp.src(["./scenes/finger_shapekeys/javascript/finger_shapekeys.js"])
+gulp.task('finger_model' ,function() {
+	path = "../scenes/QueuedInterpolation/finger_shapekeys/javascript";
+	return gulp.src([path + "/finger_model.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('five_cube_emit' ,function() {
-	return gulp.src(["./scenes/five_cube_emit/javascript/five_cube_emit.js"])
+	path = "../scenes/five_cube_emit/javascript";
+	return gulp.src([path + "/five_cube_emit.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('flying_carpet' ,function() {
-	return gulp.src(["./scenes/flying_carpet/javascript/shape_key.js"])
+	path = "../scenes/QueuedInterpolation/flying_carpet/javascript";
+	return gulp.src([path + "/shape_key.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('get_baked' ,function() {
-	return gulp.src(["./scenes/get_baked/javascript/get_baked.js"])
+	path = "../scenes/get_baked/javascript";
+	return gulp.src([path + "/get_baked.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('gus' ,function() {
-	return gulp.src(["./scenes/whoopstopia/javascript/gus.js"])
+	path = "../scenes/whoopstopia/javascript";
+	return gulp.src([path + "/gus.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('landscape' ,function() {
-	return gulp.src(["./scenes/whoopstopia/javascript/landscape.js"])
+	path = "../scenes/whoopstopia/javascript";
+	return gulp.src([path + "/landscape.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('mesh_parent' ,function() {
-	return gulp.src(["./scenes/mesh_parent/javascript/mesh_parent.js"])
+	path = "../scenes/QueuedInterpolation/mesh_parent/javascript";
+	return gulp.src([path + "/mesh_parent.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('meter_scaled' ,function() {
-	return gulp.src(["./scenes/game_rig_tester/javascript/meter_scaled.js"])
+	path = "../scenes/QueuedInterpolation/game_rig_tester/javascript";
+	return gulp.src([path + "/meter_scaled.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
-gulp.task('multi_shapekey_groups' ,function() {
-	return gulp.src(["./scenes/multi_shapekey_groups/javascript/multi_shapekey_groups.js"])
+gulp.task('multi_group' ,function() {
+	path = "../scenes/QueuedInterpolation/multi_shapekey_groups/javascript";
+	return gulp.src([path + "/multi_group.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('young_white_female' ,function() {
-	return gulp.src(["./scenes/human_emotions/javascript/young_white_female.js"])
+	path = "../scenes/Being/human_emotions/javascript";
+	return gulp.src([path + "/young_white_female.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('whoopsSign' ,function() {
-	return gulp.src(["./scenes/whoopstopia/javascript/whoopsSign.js"])
+	path = "../scenes/whoopstopia/javascript";
+	return gulp.src([path + "/whoopsSign.js"])
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
+        .pipe(optimisejs())
+        .pipe(gulp.dest(path));
 });
