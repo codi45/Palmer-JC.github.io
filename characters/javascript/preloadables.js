b315cbb8f4b709f44fc2f115b1f87611ca66eaa0
Palmer-JC <jeffrey.c.palmer@gmail.com>
2017-07-10 16:18:49 -0400

    Switch from .jpeg to .jpg extensions
    .jpg is more efficient in GPU memory by engine

--- characters/javascript/preloadables.js
+++ characters/javascript/preloadables.js
@@ -1,12 +1,12 @@
 // commented out version, for when running locally to develop a new character or set.  Only Edge allows texture file:// urls where there are  ../'s
-//var rootPath = "../../..";
-var rootPath = "https://palmer-jc.github.io";
+var rootPath = "../../..";
+//var rootPath = "https://palmer-jc.github.io";
 
 var characterJukebox = new TOWER_OF_BABEL.Preloader(rootPath + "/characters/javascript/", rootPath + "/characters/images/");
 
 characterJukebox.addCharacter(new TOWER_OF_BABEL.Character("Nate"    , "Nate.min.js", "Nate_Male_muscle_13290"));
 characterJukebox.addCharacter(new TOWER_OF_BABEL.Character("owm"     , "owm.min.js" , "Owm_Body"));
-//characterJukebox.addCharacter(new TOWER_OF_BABEL.Character("ywf"     , "ywf.min.js" , "Ywf_Female_generic"));
+characterJukebox.addCharacter(new TOWER_OF_BABEL.Character("woman"   , "woman.js" , "Woman_Body"));
 
 characterJukebox.addBust     (new TOWER_OF_BABEL.Character("NateBust", "NateBust.min.js", "Nate_Male_muscle_13290"));
 characterJukebox.addBust     (new TOWER_OF_BABEL.Character("owmBust" , "owmBust.min.js" , "Owm_Body"));
