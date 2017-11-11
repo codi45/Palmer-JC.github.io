dc16b86521f9856ee2aa55952f418251761897d4
Palmer-JC <jeffrey.c.palmer@gmail.com>
2017-11-11 17:34:49 -0500

    Head extraction scene

--- scenes/head_extraction/javascript/onAppReady.js
+++ scenes/head_extraction/javascript/onAppReady.js
@@ -1,25 +1,13 @@
 //full path so can run on chrome during dev
-var commonAudioDir = "https://palmer-jc.github.io/audio";
-var audioDir = "https://palmer-jc.github.io/scenes/blow_me_baby/audio";
-
+var canvas;
+var engine;
 var scene;
 var camera;
 var camLight;
-var initialCamPos = new BABYLON.Vector3(-30, 9, 0);
-var initialCamRot = new BABYLON.Vector3(0, Math.PI / 2, 0);
-
-var finalCamPos = new BABYLON.Vector3(0, 9, -30);
-var finalCamRot = new BABYLON.Vector3(0, 0, 0);
-
-var butterflies = new Array(94);
-var bodyDim = 1.5;
-var width = bodyDim * 5.5; // includes space between
-var leftMost = -1 * (width * 4);
-var rightMost;
-var deck = 2;
-var ceiling = deck + (bodyDim * 5);
 
 var nate;
+
+var gifMaker;
 
 function onAppReady() {
     if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
@@ -27,54 +15,25 @@ function onAppReady() {
     }
     
     if (BABYLON.Engine.isSupported()) {
-	    var canvas = document.getElementById("renderCanvas");
+    	QI.GifMaker.librariesLoad("../../lib");
+	    canvas = document.getElementById("renderCanvas");
 	    canvas.screencanvas = true; // for CocoonJS
-	    var engine = new BABYLON.Engine(canvas, true, { stencil: true });
+	    engine = new BABYLON.Engine(canvas, true, { stencil: true });
 	
 	    scene = new BABYLON.Scene(engine);
-	    scene.clearColor = new BABYLON.Color3(.5,.5,.5);
+	    scene.clearColor = new BABYLON.Color3(0,1,1);
 	    
-        camera = new BABYLON.FreeCamera("Camera", initialCamPos, scene);
-        camera.rotation = initialCamRot;
+        nate = new Nate.Nate_Male_muscle_13290("nate", scene, "../../characters/images/");
+	    nate.entranceMethod = null;
+        nate.addStockExpressions("", true);
+    	nate.removeExpressionComponents();
+    	nate.assignPoseLibrary("HD-Extraction");
+    	
+        camera = new QI.CylinderCamera("Camera", true, Math.PI, 30, scene);
+        camera.wheelPrecision = 50;	    
+        camera.attachControl(canvas);
+        camera.setTargetMesh(nate, true);
 
-        // launch GUI
-        var launchTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-        var launchPanel = new BABYLON.GUI.StackPanel();
-        launchPanel.width = "200px";
-        launchPanel.isVertical = true;
-        launchPanel.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
-        launchPanel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER;
-        launchTexture.addControl(launchPanel);
-
-        var launchBtn = BABYLON.GUI.Button.CreateSimpleButton("launchBtn", "Launch");
-        launchBtn.width = "150px"
-        launchBtn.height = "40px";
-        launchBtn.color = "white";
-        launchBtn.cornerRadius = 20;
-        launchBtn.background = "orange";
-        launchBtn.onPointerUpObservable.add(function() {
-        	launchTexture.dispose();
-            launch();
-        });
-        launchPanel.addControl(launchBtn);    
-	    
-	    engine.runRenderLoop(function () {
-	        scene.render();
-	    });
-	    
-	    preloading();
-	    
-    }else{
-        alert("WebGL not supported in this browser.");
-    }
-
-    //Resize
-    window.addEventListener("resize", function () {
-        engine.resize();
-    })
-}
-
-function launch() {
         camlight = new BABYLON.PointLight("camLight", BABYLON.Vector3.Zero(), scene);        
         // still works if the active camera gets switched out; not used till later
         scene.beforeCameraRender = function () {
@@ -84,10 +43,6 @@ function launch() {
             camlight.rotation = cam.rotation;
         }; 
         
-	    nate.setEnabled(true);
-	    nate.entranceMethod = null;
-	    nate.grandEntrance();
-	    
 	    var ground = BABYLON.Mesh.CreatePlane("ground", 1000, scene);
 	    ground.rotation.x = Math.PI / 2;
 	    ground.material = new BABYLON.ShadowOnlyMaterial('mat', scene)
@@ -102,41 +57,86 @@ function launch() {
         shadowGenerator.setDarkness(0.1);
         freshenShadowRenderLists(scene);   
         
- //       QI.TimelineControl.Speed = .1;
-        var camDuration = 0;
-        nate.queuePose("extraction-2", 500, null, null, {millisBefore: 2000}); camDuration += 500;
-        nate.queuePose("extraction-3", 400); camDuration += 400;
-        nate.queuePose("extraction-4", 400); camDuration += 400;
-        nate.queuePose("extraction-5", 400); camDuration += 500;
-        nate.queuePose("extraction-6", 500); camDuration += 500;
-        nate.queuePose("extraction-7", 800); camDuration += 800;
-        nate.queuePose("extraction-8", 500);
-        nate.queuePose("extraction-9", 800);
-        nate.queuePose("extraction-8", 900);
-        nate.queuePose("do-not", 400);
-        nate.queuePose("go-in-there", 200);
-        
-        doCameraAnimation(camDuration, 2000);
+        createControlPanel();
+            	
+        engine.runRenderLoop(function () {
+	        scene.render();
+	    });
+	    
+    }else{
+        alert("WebGL not supported in this browser.");
+    }
 
+    //Resize
+    window.addEventListener("resize", function () {
+        engine.resize();
+    })
 }
-//======================================= Pre-loading =========================================
-var windSnd;
-var whatTheSnd;
-var clickSnd;
-function preloading() {
-//	windSnd    = new BABYLON.Sound("wind"    , commonAudioDir + "/wind.mp3", scene, null, {loop: true});
-//	whatTheSnd = new BABYLON.Sound("what_the", audioDir       + "/what_the.mp3", scene);
-//	clickSnd   = new BABYLON.Sound("click"   , commonAudioDir + "/click.mp3", scene);
-    nate = new Nate.Nate_Male_muscle_13290("nate", scene, "../../characters/images/");
-    nate.setEnabled(false);
-    nate.addStockExpressions("", true);
-	nate.removeExpressionComponents();
-	nate.assignPoseLibrary("HD-Extraction");
-    nate.assignPoseImmediately("extraction-1");
-    nate.deformImmediately("R_HAND", "FINGERS_TOGETHER");
-    nate.deformImmediately("L_HAND", "FINGERS_TOGETHER");
 
-	nate.compileMaterials();
+function createControlPanel() {
+    var cntrlPnlTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("cntrlPnlTexture");
+
+    var controlPanel = new BABYLON.GUI.StackPanel();
+    controlPanel.width = "200px";
+    controlPanel.height = "300px";
+    controlPanel.isVertical = true;
+    controlPanel.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+    controlPanel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+    cntrlPnlTexture.addControl(controlPanel);
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    playBtn = BABYLON.GUI.Button.CreateSimpleButton("playBtn", "Play");
+    playBtn.width = "150px";
+    playBtn.height = "40px";
+    playBtn.cornerRadius = 5;
+    playBtn.color = "white";
+    playBtn.background = "Maroon";
+    playBtn.paddingLeft = 5;
+    playBtn.onPointerUpObservable.add(function() {
+    	var makeGIF = makeGIFCheck.isChecked;
+    	if (makeGIF) cntrlPnlTexture.dispose();
+        launch(makeGIF);
+    });
+    controlPanel.addControl(playBtn);
+    
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    var makeGIFCheck = new BABYLON.GUI.Checkbox();
+    makeGIFCheck.width = "20px";
+    makeGIFCheck.height = "20px";
+    makeGIFCheck.isChecked = false;
+    makeGIFCheck.color = "red";
+
+    var panelForCheckbox = BABYLON.GUI.Control.AddHeader(makeGIFCheck, "Make GIF", "180px", { isHorizontal: true, controlFirst: true});
+    panelForCheckbox.color = "black";
+    panelForCheckbox.height = "20px";
+    panelForCheckbox.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;        
+    controlPanel.addControl(panelForCheckbox);
+}
+
+function launch(makeGIF) {
+	    if (makeGIF) {
+	    	gifMaker = new QI.GifMaker("extraction.gif", engine, 25);
+	    }
+
+        nate.assignPoseImmediately("extraction-1");
+        camera.alpha = Math.PI;
+	    
+        var delay = 500;
+        var camDelay = delay;
+        nate.queuePose("extraction-2", 200, null, null, {millisBefore: delay}); camDelay += 175;
+        nate.queuePose("extraction-3", 150); camDelay += 150;
+        nate.queuePose("extraction-4", 150); camDelay += 150;
+        nate.queuePose("extraction-5", 150); camDelay += 150;
+        nate.queuePose("extraction-6", 175); camDelay += 175;
+        nate.queuePose("extraction-7", 230); camDelay += 230;
+        nate.queuePose("extraction-8", 150);
+//        nate.queuePose("extraction-9", 800, null, null, {millisBefore: 1000});        
+//        nate.queuePose("extraction-8", 800);
+ //       var doNot = nate.queuePose("do-not", 400);
+ //       nate.queuePose("go-in-there", 200);
+        if (makeGIF) nate.queueFunction(function() { gifMaker.finish(); }, QI.PoseProcessor.INTERPOLATOR_GROUP_NAME);
+       
+        doCameraAnimation(camDelay);
 }
 
 //from a TOB export with initScene
@@ -153,15 +153,11 @@ function freshenShadowRenderLists(scene) {
     }
 }
 
-function doCameraAnimation(duration, delay) {
+function doCameraAnimation(delay) {
     var cameraQueue = new QI.PovProcessor(camera); 
-
-    cameraQueue.queueSingleEvent(
-    		new QI.MotionEvent(duration, finalCamPos, finalCamRot, { millisBefore: delay, absoluteMovement : true, absoluteRotation: true } )
-    );
+    var event = new QI.PropertyEvent(camera, "alpha", Math.PI * 1.4, 2000, { millisBefore: delay } );
+    cameraQueue.queueSingleEvent(event);
 }
-
-
 
 // this is an XDK event; when not using XDK, place in <body onload="onAppReady()">
 document.addEventListener("app.Ready", onAppReady, false) ;
